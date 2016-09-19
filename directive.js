516aa05c6f3c7e9ad5a69e520cd0d9314b05cf65
asemle <asemle@umass.edu>
2016-09-19 16:21:35 -0500

    deleted directive

--- directive.js
+++ directive.js
@@ -1,12 +1,1 @@
-angular.module('pensieve')
-.directive('myDirective', function() {
-   return {
-      template: '<div class="navBar"></div>'
-       templateUrl: "./templates/stream.html",
-       link: function ($scope, element, attrs) {
-           if($scope.streamed) {function() {
-               element.css('opacity:1');
-   }
-}
-}
-})
+
